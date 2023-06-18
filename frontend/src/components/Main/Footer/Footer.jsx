c24356f8db54e0eae807572b165d8cd9786d22a1
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-06-19 01:40:01 +0400

    footer

--- frontend/src/components/Main/Footer/Footer.jsx
+++ frontend/src/components/Main/Footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import style from '../../Main/Footer/footer.module.css'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import EmailIcon from '@mui/icons-material/Email';
+import LanguageIcon from '@mui/icons-material/Language';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer>
+        <div className='container'>
+          <div className='row'>
+            <div className={style.centerfooter}>
+              <div className={style.leftfooter}>
+                <h5>CONTACT US</h5>
+                <p>PO Box 16122 Collins Victoria 3000 Australia</p>
+                <span>Email: info@chiart.abcdf.com</span>
+                <br></br>
+                <span>Phone: + 33 323 34522</span>
+                <span>Fax: +33 3467888</span>
+              </div>
+              <div className='footerlogo'>
+                <img src='' alt='' />
+              </div>
+              <div className={style.rightfooter}>
+                <h5>ABOUT SAY</h5>
+                <p>“ Morbi mollis vestibulum sollicitudin. Nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet ”</p>
+                <span>JOIN DOER COOK</span>
+                <br></br>
+                <span>Ceo Bakery</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.icons}>
+          <FacebookOutlinedIcon />
+          <TwitterIcon />
+          <YouTubeIcon/>
+          <EmailIcon/>
+          <LanguageIcon/>
+          <p>Copyright © 2023 Bakery – Design by ArrowHitech – All Rights Reserved</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
--- frontend/src/components/Main/Navbar/Navbar.jsx
+++ frontend/src/components/Main/Navbar/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import style from '../../Main/Navbar/navbar.module.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div className='container'>
+        <div className='row'>
+          <div className={style.centernav}>
+            <div className={style.leftnav}>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+                <li>
+                  <Link to='/servies'>Services</Link>
+                </li>
+              </ul>
+            </div>
+            <div className='navlogo'>
+              <img src='' alt='' />
+            </div>
+            <div className='rightnav'>
+              <ul>
+                <li>
+                  <Link to='/shop'>Shop</Link>
+                </li>
+                <li>
+                    <Link to='/blog'>Blog</Link>
+                </li>
+                <li>
+                     <Link to='/contact'>Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
