8061837924d5e31c73cd786c0323d38d4ec091fd
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-06-20 22:23:27 +0400

    home static

--- frontend/src/pages/Main/Home/Home.jsx
+++ frontend/src/pages/Main/Home/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import style from '../Home/home.module.css'
+import icon from '../../../images/icon.png'
 const Home = () => {
   return (
     <>
-    <h1>Home page</h1>
+      <main>
+        <section className={style.hero}>
+          <div className={style.home}>
+            <h5>WELCOME TO</h5>
+            <h1>FRESH BAKERY</h1>
+            <h6>Dicovery The Amazing Bakery</h6>
+          </div>
+        </section>
+        <section className={style.sect2}>
+          <img src={icon} alt=''/>
+          <h2>FRESH SERVICES</h2>
+          <h6>The Best Of Our Services</h6>
+        </section>
+      </main>
     </>
   )
 }
