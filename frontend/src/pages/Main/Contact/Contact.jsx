028852ab9fad3d051d2e415970ace52ec661a407
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-06-26 15:54:34 +0400

    contact page

--- frontend/src/pages/Main/Contact/Contact.jsx
+++ frontend/src/pages/Main/Contact/Contact.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import style from '../Contact/contact.module.css'
 
 const Contact = () => {
   return (
     <>
-    <main>
-      <section className='contact'>
-        <div className='contactwrite'>
-          <h1>CONTACT</h1>
-          <p>Amazing Bakery Kit</p>
-        </div>
-      </section>
-    </main>
+      <main>
+        <section className={style.contact}>
+          <div className={style.contactwrite}>
+            <h1>CONTACT</h1>
+            <p>Amazing Bakery Kit</p>
+            <p>Di Cantina Valpolicella Negrar è la storia di uomini e donne dediti
+              <br></br> alla Nullam dui dolor, sagittis ut ante eget Aliquam eratvolutpat.
+              <br></br>Vivamus tincidunt justo in eros cursus, et viverra elit dignissim.
+              <br></br>Nulla facilisi</p>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
--- frontend/src/pages/Main/Home/Home.jsx
+++ frontend/src/pages/Main/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from '../Home/home.module.css'
 import icon from '../../../images/icon.png'
+import icon5 from '../../../images/icon-5.png'
 const Home = () => {
   return (
     <>
@@ -13,9 +14,22 @@ const Home = () => {
           </div>
         </section>
         <section className={style.sect2}>
-          <img src={icon} alt=''/>
+          <img src={icon} alt='' />
           <h2>FRESH SERVICES</h2>
           <h6>The Best Of Our Services</h6>
+        </section>
+        <section className={style.testimonals}>
+          <h2>TESTIMONALS</h2>
+          <h6>What Clients Say About Us</h6>
+        </section>
+        <section className={style.care}>
+          <img src={icon5} alt='' />
+          <h2>BAKERY CARE</h2>
+          <h6>A Few Of Our Favourite Things</h6>
+        </section>
+        <section className={style.news}>
+          <h2>NEWS & BLOG</h2>
+          <h6>A Love Story From The Blog</h6>
         </section>
       </main>
     </>
