bf3acdb88cd3c2565403d7246df3dac0e31cef9b
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-07-05 00:52:34 +0400

    some footer problems

--- frontend/src/components/Main/Footer/Footer.jsx
+++ frontend/src/components/Main/Footer/Footer.jsx
@@ -11,37 +11,37 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container'>
-          <div className='row'>
-            <div className={style.centerfooter}>
-              <div className={style.leftfooter}>
-                <h5>CONTACT US</h5>
-                <p>PO Box 16122 Collins Victoria 3000 Australia</p>
-                <span>Email: info@chiart.abcdf.com</span>
-                <br></br>
-                <span>Phone: + 33 323 34522</span>
-                <span>Fax: +33 3467888</span>
-              </div>
-              <div className={style.footerlogo}>
-                <img src={logo} alt='' />
-              </div>
-              <div className={style.rightfooter}>
-                <h5>ABOUT SAY</h5>
-                <p>“ Morbi mollis vestibulum sollicitudin. Nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet ”</p>
-                <span>JOIN DOER COOK</span>
-                <br></br>
-                <span>Ceo Bakery</span>
+        <div className={style.footerbackground}>
+          <div className={style.container}>
+            <div className='row'>
+              <div className={style.centerfooter}>
+                <div className={style.leftfooter}>
+                  <h5>CONTACT US</h5>
+                  <p>PO Box 16122 Collins Victoria 3000 Australia</p>
+                  <span>Email: info@chiart.abcdf.com</span>
+                  <br></br>
+                  <span>Phone: + 33 323 34522</span>
+                  <br></br>
+                  <span>Fax: +33 3467888</span>
+                </div>
+                <div className={style.footerlogo}>
+                  <img src={logo} alt='' />
+                </div>
+                <div className={style.rightfooter}>
+                  <h5>ABOUT SAY</h5>
+                  <p>“ Morbi mollis vestibulum sollicitudin. Nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet ”</p>
+                  <p>JOIN DOER COOK</p>
+                  <br></br>
+                  <p>Ceo Bakery</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={style.icons}>
-          <ul>
-           
+          <div className={style.icons}>
+            <ul>
               <li>
-                 <FacebookOutlinedIcon />
+                <FacebookOutlinedIcon />
               </li>
-             
               <li>
                 <TwitterIcon />
               </li>
@@ -54,13 +54,9 @@ const Footer = () => {
               <li>
                 <LanguageIcon />
               </li>
-
-           
-          </ul>
-
-
-
-          <p>Copyright © 2023 Bakery – Design by ArrowHitech – All Rights Reserved</p>
+            </ul>
+            <p>Copyright © 2023 Bakery – Design by ArrowHitech – All Rights Reserved</p>
+          </div>
         </div>
       </footer>
     </>
