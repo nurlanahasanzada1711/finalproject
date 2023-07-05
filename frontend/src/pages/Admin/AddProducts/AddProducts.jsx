2b3b934c232b037cd4c2576920a2e3d0db6e4b36
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-07-05 22:02:00 +0400

    admin panel add product ready

--- frontend/src/pages/Admin/AddProducts/AddProducts.jsx
+++ frontend/src/pages/Admin/AddProducts/AddProducts.jsx
@@ -1,8 +1,101 @@
 import React from 'react'
+import { useFormik } from "formik";
+import { postProducts } from "../../../api/productrequests";
+import Swal from "sweetalert2";
+import style from '../AddServices/addservice.module.css'
+import * as yup from 'yup';
 
 const AddProducts = () => {
+
+  const Validation = yup.object().shape({
+    price: yup.number().required('Please fill in the box'),
+    desc: yup.string().required('Please fill in the box'),
+    imageURL: yup.string().required('Please fill in the box'),
+  })
+  const handleSubmit = async (values, actions) => {
+    await postProducts(values)
+    console.log(values);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: `${values.service} posted successfully!`,
+      showConfirmButton: false,
+      timer: 1500
+    })
+    actions.resetForm()
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      price: '',
+      desc: '',
+      imageURL: '',
+    },
+    onSubmit: handleSubmit,
+    validationSchema: Validation,
+  })
   return (
-    <div>AddProducts</div>
+    <>
+       <div className={style.addlogin}>
+        <h2>Add New Products</h2>
+        <form onSubmit={formik.handleSubmit}>
+          <div style={{ width: "40%", margin: "15px auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <input
+              style={{ padding: "15px" }}
+              placeholder='Add product image'
+              type="text"
+              name="imageURL"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.imageURL}
+            />
+            {formik.errors.imageURL && formik.touched.imageURL ? (
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.imageURL}</span>
+            ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
+          </div>
+          <div style={{ width: "40%", margin: "15px auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <input
+              style={{ padding: "15px" }}
+              placeholder='Add new product name'
+              type="text"
+              name="desc"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.desc}
+            />
+            {formik.errors.desc && formik.touched.desc ? (
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.desc}</span>
+            ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
+          </div>
+          <div style={{ width: "40%", margin: "0 auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <input
+              style={{ padding: "15px" }}
+              placeholder='Price'
+              type="number"
+              name="price"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.price}
+
+            />
+            {formik.errors.price && formik.touched.price ? (
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.price}</span>
+            ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
+          </div>
+              
+          <div className={style.addbutton}>
+            <button
+            
+              disabled={formik.isSubmitting || Object.keys(formik.errors).length > 0 ? true : false}
+              type="submit"
+              style={{ cursor: "pointer", marginTop: "20px" }}
+              variant="contained"
+              
+            >Add</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/Admin/AddServices/AddServices.jsx
+++ frontend/src/pages/Admin/AddServices/AddServices.jsx
@@ -7,14 +7,20 @@ import * as yup from 'yup';
 
 const AddServices = () => {
   const Validation = yup.object().shape({
-    about: yup.string().required('Zəhmət olmasa xananı doldurun'),
-    desc: yup.string().required('Zəhmət olmasa xananı doldurun'),
-    imageURL: yup.string().required('Zəhmət olmasa xananı doldurun'),
+    about: yup.string().required('Please fill in the box'),
+    desc: yup.string().required('Please fill in the box'),
+    imageURL: yup.string().required('Please fill in the box'),
   })
   const handleSubmit = async (values, actions) => {
     await postServices(values)
     console.log(values);
-
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: `${values.service} posted successfully!`,
+      showConfirmButton: false,
+      timer: 1500
+    })
     actions.resetForm()
   }
 
@@ -27,41 +33,16 @@ const AddServices = () => {
     onSubmit: handleSubmit,
     validationSchema: Validation,
   })
-  // const navigate = useNavigate();
-  // const [service, setService] = useState([]);
-  // const handleSubmit = async (values, actions) => {
-  //   await postServices(values);
-  //   console.log(values);
-
-  //   setService([...service, values])
-  //   Swal.fire({
-  //     position: 'top-end',
-  //     icon: 'success',
-  //     title: `${values.name} posted successfully!`,
-  //     showConfirmButton: false,
-  //     timer: 1500
-  //   })
-  //   actions.resetForm();
-  //   navigate('/admin/services');
-  // };
-  // const formik = useFormik({
-  //   initialValues: {
-  //     desc: "",
-  //     imageURL: "",
-  //     about: "",
-  //   },
-  //   validationSchema: ServicesValidation,
-  //   onSubmit: handleSubmit,
-  // });
+ 
   return (
     <>
       <div className={style.addlogin}>
         <h2>Add New Service</h2>
         <form onSubmit={formik.handleSubmit}>
-          <div style={{ width: "40%", margin: "0 auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+          <div style={{ width: "40%", margin: "15px auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
             <input
-              style={{ padding: "10px" }}
-              placeholder='Kategoriya adı'
+              style={{ padding: "15px" }}
+              placeholder='About new service'
               type="text"
               name="about"
               onChange={formik.handleChange}
@@ -69,13 +50,13 @@ const AddServices = () => {
               value={formik.values.about}
             />
             {formik.errors.about && formik.touched.about ? (
-              <span style={{ color: "#bb221a", marginTop: "15px" }}>{formik.errors.about}</span>
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.about}</span>
             ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
           </div>
-          <div style={{ width: "40%", margin: "0 auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+          <div style={{ width: "40%", margin: "15px auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
             <input
-              style={{ padding: "10px" }}
-              placeholder='Kategoriya adı'
+              style={{ padding: "15px" }}
+              placeholder='Add new service'
               type="text"
               name="desc"
               onChange={formik.handleChange}
@@ -83,72 +64,35 @@ const AddServices = () => {
               value={formik.values.desc}
             />
             {formik.errors.desc && formik.touched.desc ? (
-              <span style={{ color: "#bb221a", marginTop: "15px" }}>{formik.errors.desc}</span>
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.desc}</span>
             ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
           </div>
           <div style={{ width: "40%", margin: "0 auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
             <input
-              style={{ padding: "10px" }}
-              placeholder='Kategoriya adı'
+              style={{ padding: "15px" }}
+              placeholder='Add Image'
               type="text"
               name="imageURL"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.imageURL}
+              variant='standard'
+
             />
             {formik.errors.imageURL && formik.touched.imageURL ? (
-              <span style={{ color: "#bb221a", marginTop: "15px" }}>{formik.errors.imageURL}</span>
+              <span style={{ color: "#7E5A4E", marginTop: "15px" }}>{formik.errors.imageURL}</span>
             ) : <span style={{ visibility: "hidden", marginTop: "15px" }}>error message</span>}
           </div>
-          {/* <div class="user-box">
-            <input
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.imageURL}
-              name="imageURL"
-              type="text"
-              placeholder="imageURL"
-            />
-
-            {formik.errors.imageURL && formik.touched.imageURL && (
-              <span>{formik.errors.imageURL}</span>
-            )}
-          </div>
-          <div class="user-box">
-            <input
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.desc}
-              name="desc"
-              type="text"
-              placeholder="desc"
-            />
-
-            {formik.errors.desc && formik.touched.desc && (
-              <span>{formik.errors.desc}</span>
-            )}
-          </div>
-          <div class="user-box">
-            <input
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.about}
-              name="about"
-              type="text"
-              placeholder="about"
-            />
-
-            {formik.errors.about && formik.touched.about && (
-              <span>{formik.errors.about}</span>
-            )}
-          </div> */}
-          <div className='product-send'>
+              
+          <div className={style.addbutton}>
             <button
+            
               disabled={formik.isSubmitting || Object.keys(formik.errors).length > 0 ? true : false}
               type="submit"
               style={{ cursor: "pointer", marginTop: "20px" }}
               variant="contained"
-            >Göndər</button>
+              
+            >Add</button>
           </div>
         </form>
       </div>
